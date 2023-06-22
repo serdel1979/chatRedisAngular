40e279be76b2f1ef9e355563774f543d7e1cbca2
sergio <sdlbsso@gmail.com>
2023-06-22 12:24:20 -0300

    envio y recepcion de mensajes guardados en redis

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'chat-app';
+
+
+  messages: string[] = [];
+  newMessage: string = '';
+
+  constructor(private http: HttpClient){}
+
+  ngOnInit() {
+    this.loadMessages();
+  }
+
+  loadMessages() {
+    this.http.get<string[]>('https://localhost:7078/chat').subscribe((response) => {
+      this.messages = response;
+    });
+  }
+
+  sendMessage() {
+    const msg = {
+      Message : this.newMessage
+    }
+    this.http.post('https://localhost:7078/chat', msg).subscribe(() => {
+      this.newMessage = '';
+      this.loadMessages();
+    });
+  }
 }
